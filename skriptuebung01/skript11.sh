921b6e2a35afdfccc7fa9e79669817cd6372758b
Hubi <constantine17@gmail.com>
2013-06-03 00:43:20 +0200

    updated skript11.sh

--- skriptuebung01/skript11.sh
+++ skriptuebung01/skript11.sh
@@ -2,6 +2,9 @@
 
 if [[ "$1" != "" ]]
 then
-	zu=`crypt($1,$1$12345678)`
+	# mit dem aufruf geht es bei mir unter arch nicht
+	#zu=`crypt($1,$1$12345678)`
+	# aber mit folgender aenderung laeufts
+	zu=$(perl -e 'print crypt($1, "password")' $1)
 	echo $zu
 fi
